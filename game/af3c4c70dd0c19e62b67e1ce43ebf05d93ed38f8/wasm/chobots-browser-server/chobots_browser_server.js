6b1ee9b5f928e3554e5dba4abd8d1292e5ae2b70
RaedwaldR <will+kavalok@rich.es>
2026-08-29 22:10:54 +0100

    Stage game release af3c4c70dd0c19e62b67e1ce43ebf05d93ed38f8 before activation

--- game/af3c4c70dd0c19e62b67e1ce43ebf05d93ed38f8/wasm/chobots-browser-server/chobots_browser_server.js
+++ game/af3c4c70dd0c19e62b67e1ce43ebf05d93ed38f8/wasm/chobots-browser-server/chobots_browser_server.js
@@ -467,7 +467,7 @@ function __wbg_get_imports() {
                     const a = state0.a;
                     state0.a = 0;
                     try {
-                        return __wasm_bindgen_func_elem_11371(a, state0.b, arg0, arg1);
+                        return __wasm_bindgen_func_elem_11375(a, state0.b, arg0, arg1);
                     } finally {
                         state0.a = a;
                     }
@@ -631,7 +631,7 @@ function __wbg_get_imports() {
         }, arguments); },
         __wbindgen_cast_0000000000000001: function(arg0, arg1) {
             // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 1603, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
-            const ret = makeMutClosure(arg0, arg1, __wasm_bindgen_func_elem_11368);
+            const ret = makeMutClosure(arg0, arg1, __wasm_bindgen_func_elem_11372);
             return addHeapObject(ret);
         },
         __wbindgen_cast_0000000000000002: function(arg0) {
@@ -673,10 +673,10 @@ function __wbg_get_imports() {
     };
 }
 
-function __wasm_bindgen_func_elem_11368(arg0, arg1, arg2) {
+function __wasm_bindgen_func_elem_11372(arg0, arg1, arg2) {
     try {
         const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
-        wasm.__wasm_bindgen_func_elem_11368(retptr, arg0, arg1, addHeapObject(arg2));
+        wasm.__wasm_bindgen_func_elem_11372(retptr, arg0, arg1, addHeapObject(arg2));
         var r0 = getDataViewMemory0().getInt32(retptr + 4 * 0, true);
         var r1 = getDataViewMemory0().getInt32(retptr + 4 * 1, true);
         if (r1) {
@@ -687,8 +687,8 @@ function __wasm_bindgen_func_elem_11368(arg0, arg1, arg2) {
     }
 }
 
-function __wasm_bindgen_func_elem_11371(arg0, arg1, arg2, arg3) {
-    wasm.__wasm_bindgen_func_elem_11371(arg0, arg1, addHeapObject(arg2), addHeapObject(arg3));
+function __wasm_bindgen_func_elem_11375(arg0, arg1, arg2, arg3) {
+    wasm.__wasm_bindgen_func_elem_11375(arg0, arg1, addHeapObject(arg2), addHeapObject(arg3));
 }
 
 const BrowserServerFinalization = (typeof FinalizationRegistry === 'undefined')
